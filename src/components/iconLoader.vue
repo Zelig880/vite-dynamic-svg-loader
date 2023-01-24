b13f4c1005c089229097b75be79b45b1758c9433
Simone Cuomo <simone@thisdot.co>
2023-01-24 22:40:11 +0000

    First commit

---
+++ src/components/iconLoader.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import { ref } from 'vue'
+    const props = defineProps({
+        icon: String
+    })
+    const iconName = ref(null);
+    
+    import(`../assets/${props.icon}.svg`)
+      .then((module) => {
+        iconName.value = module.default;
+      })
+      .catch(console.error.bind(console));
+</script>
+
+<template>
+    <component :is="iconName" />
+</template>
